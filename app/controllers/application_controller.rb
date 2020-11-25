39176681d6c09a58ac47be81f288cfa2ca9b5877
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-25 13:15:43 -0300

    Creating Users CRUD and all requests test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::API
   include Pundit
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  include SimpleErrorRenderable
+  self.simple_error_partial = 'shared/simple_error'
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,45 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :load_user, only: %i[update destroy]
+
   def index
     @users = User.all
   end
   
+  def create
+    @user = User.new
+    @user.attributes = user_params
+    save_user!
+  end
+
+  def update
+    @user.attributes = user_params
+    save_user!
+  end
+
+  def destroy
+    @user.destroy!
+  rescue StandardError
+    render_error(fields: @user.errors.messages)
+  end
+
+  private
+
+  def user_params
+    return {} unless params.has_key?(:user)
+
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role)
+  end
+
+  def load_user
+    @user = User.find(params[:id])
+  end
+
+  def save_user!
+    @user.save!
+    render :show
+  rescue StandardError
+    render_error(fields: @user.errors.messages)
+  end
+
 end
