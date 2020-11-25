ff5cec9597ab89cb8840965beb2c1e0a4a9de3b9
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-25 16:50:25 -0300

    Creating unathenticated requests tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'shared_examples', '**', '*.rb')].each { |f| require f } 
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
---
+++ spec/requests/users/unauthenticated_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Users without authentication' do
+
+  context 'GET /users' do
+    let(:url) { '/users' }
+    let(:users) { create_list(:user, 5) }
+    before(:each) { get url }
+    include_examples 'unauthenticated access'
+  end
+
+  context 'POST /users' do
+    let(:url) { '/users' }
+    before(:each) { post url }
+    include_examples 'unauthenticated access'
+  end
+
+  context 'PATCH /users/:id' do
+    let(:user) { create(:user) }
+    let(:url) { "/users/#{user.id}" }
+    before(:each) { patch url }
+    include_examples 'unauthenticated access'
+  end
+
+  context 'DELETE /users/:id' do
+    let(:user) { create(:user) }
+    let(:url) { "/users/#{user.id}" }
+    before(:each) { delete url }
+    include_examples 'unauthenticated access'
+  end
+end
