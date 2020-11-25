ce2f4cf2d74f6bad62326443998e0c42899e1ba8
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-25 09:20:10 -0300

    Using RSpec to test User Model and validating it afterwards

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true  
+
+  validates :role, presence: true
+  enum role: { admin: 0, client: 1 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:first_name) }
+  it { is_expected.to validate_presence_of(:last_name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:password) }
+  it { is_expected.to validate_presence_of(:password_confirmation) }
+
+  it { is_expected.to validate_presence_of(:role) }
+  it { is_expected.to define_enum_for(:role).with_values({ admin: 0, client: 1 }) }
 end
