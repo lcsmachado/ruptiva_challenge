70c605d911658062f78a446739f31dfd370e50b7
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-27 16:50:40 -0300

    Fixing POST /users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   def user_params
     return {} unless params.has_key?(:user)
     byebug
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role)
+    params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :role)
   end
 
   def load_user
